ae0c56f99f417cf2b044a763a2ad80225855a08a
paradox <dokingkns0909@naver.com>
2024-07-15 09:16:36 +0900

    block_test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct App {
-    pub blocks: Vec<Block>,  // Vec<Block>으로 수정
+    pub blocks: Vec<Block>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -16,7 +16,6 @@ pub struct Block {
 }
 
 fn main() {
-    // 예제 데이터 생성
     let block1 = Block {
         id: 1,
         hash: String::from("hash1"),
@@ -38,11 +37,9 @@ fn main() {
         blocks: vec![block1, block2],
     };
 
-    // JSON 직렬화 예제
     let serialized = serde_json::to_string(&app).unwrap();
     println!("Serialized: {}", serialized);
 
-    // JSON 역직렬화 예제
     let deserialized: App = serde_json::from_str(&serialized).unwrap();
     println!("Deserialized: {:?}", deserialized);
 }
